18f3eaba1901fac5edea1ffe7833d849e97de97f
0verflowed-stack <spaydermendima@gmail.com>
2023-11-08 01:24:02 +0200

    Change interface of one method in utils

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ import numpy as np
 def calculate_max_relative_error(u_pred, u_exact):
     return 100 * np.linalg.norm(u_exact - u_pred, np.inf) / np.linalg.norm(u_exact, np.inf)
 
-def save_and_load_weights(model_In, layers):
+def save_and_load_weights(modelClass, model, layers, fileName):
     pass
